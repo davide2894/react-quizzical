5a2da975375cb9b06aa48b3d0489905674984b3c
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-07-11 23:43:20 +0200

    wip

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -1,6 +1,41 @@
+import { nanoid } from "nanoid";
+import { useEffect } from "react";
+import {useState} from "react";
+import Question from "../question/Question";
+
 function Quiz() {
+
+  const [quizQuestions, setQuizQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=5')
+    .then(response => {
+      if(response.ok) {
+        return response.json();
+      }
+      throw response;
+    })
+    .then(data => {
+      console.log(data.results);
+      return setQuizQuestions(data.results);
+    })
+    .catch(err => {
+      console.log("error: ", err);
+    })
+  }, [])
+
+  useEffect(() => {
+    console.log(quizQuestions);
+  }, [quizQuestions]);
+
   return (
-    <div>Quiz</div>
+    <div className='quiz'>
+      <div>Quiz</div>
+      {quizQuestions.map(question => {
+        console.log(question.question);
+        return <Question key={nanoid()} questionData={question} />
+      })}
+    </div>    
   )
 }
 
