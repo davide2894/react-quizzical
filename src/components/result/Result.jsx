4eda56be5854025a4373822d31bf3af0f6f90e03
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-07-24 18:01:05 +0200

    final style

--- src/components/result/Result.jsx
+++ src/components/result/Result.jsx
@@ -1,13 +1,13 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import "./Result.scss";
 
 function Result(props) {
   return (
     <div className="result">
         <p className="result__score">You scored {props.numberOfCorrectAnswers()}/{props.totalQuestions} correct answers</p>
-        <Link to="/">Play Again</Link>
+        <Link className="result__button" to="/">Play Again</Link>
     </div>
   )
 }
 
-export default Result
+export default Result;
