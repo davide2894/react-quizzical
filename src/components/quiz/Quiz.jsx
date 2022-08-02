6b6049580d87c5e3793759c38c343e00bd5534f9
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-08-02 18:15:40 +0200

    separate utilities

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -1,100 +1,17 @@
 import { nanoid } from "nanoid";
 import { useEffect } from "react";
 import {useState} from "react";
+import { mapQuestionsFromApi } from "../../utilities/utility"
 import Question from "../question/Question";
-import { decode } from "html-entities";
 import Result from "../result/Result";
 import "./Quiz.scss";
 
 function Quiz() {
 
-  const [ questions, setQuestions ] = useState([]);
-  const [ showResult, setShowResult ] = useState(false);
+const [ questions, setQuestions ] = useState([]);
+const [ showResult, setShowResult ] = useState(false);
 
-  function mapQuestionsFromApi(questionsData) {
-    const questionsFromApi = questionsData.results;
-    const mappedQuestions = [];
-
-    questionsFromApi.forEach(questionFromApi => {      
-      const decodedQuestion = getDecodeString(questionFromApi.question);
-      const decodedIncorrectAnswers = questionFromApi.incorrect_answers.map(incorrectAnswer => {
-        return getDecodeString(incorrectAnswer);
-      })
-      const decodedCorrectAnswer = questionFromApi.correct_answer = getDecodeString(questionFromApi.correct_answer);
-      mappedQuestions.push({
-        question: decodedQuestion,
-        choices: prepareChoices(decodedIncorrectAnswers, decodedCorrectAnswer),
-        isAnswered: false,
-        isAnsweredCorrectly: false,
-        showAnswer: false,
-        id: nanoid()
-      })
-    })
-
-    return mappedQuestions;
-  }
-
-  function getDecodeString(str) {
-    return decode(str);
-  }
-
-  function prepareChoices(incorrectAnswers, correctAnswer) {
-    let arr = [];
-
-    incorrectAnswers.forEach(incorrectAnswer => {
-        arr.push(
-            {
-                choice: incorrectAnswer, 
-                isSelected: false,
-                isCorrectChoice: false,
-                id: nanoid(),
-            });
-        }
-    );
-    
-    arr.push(
-        {
-            choice: correctAnswer, 
-            isSelected: false,
-            isCorrectChoice: true,
-            id: nanoid(),
-        }
-    );
-
-    shuffleArray(arr);
-
-    return arr;
-  }
-
-  // using the Fisher-Yates algorith in combination with Math.Random()
-  // see: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  function shuffleArray(arr) {
-    for (let i = arr.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
-    }
-  }
-
-  useEffect(() => {
-    fetch('https://opentdb.com/api.php?amount=5')
-    .then(response => {
-      if(response.ok) {
-        return response.json();
-      }
-      throw response;
-    })
-    .then(questionsData => {
-      setQuestions(mapQuestionsFromApi(questionsData));
-    })
-    .catch(err => {
-      console.log("error: ", err);
-    })
-  }, [])
-
-  function questionClickHanlder(evt, answeredQuestionId, selectedChoice){
+function questionClickHanlder(evt, answeredQuestionId, selectedChoice){
     setQuestions(oldQuestions => {
         return oldQuestions.map(oldQuestion => {
           if(oldQuestion.id === answeredQuestionId){
@@ -142,6 +59,22 @@ function Quiz() {
   }
 
   var quizClassNames=`quiz${showResult ? " quiz--complete" : ""}`;
+
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=5')
+    .then(response => {
+      if(response.ok) {
+        return response.json();
+      }
+      throw response;
+    })
+    .then(questionsData => {
+      setQuestions(mapQuestionsFromApi(questionsData));
+    })
+    .catch(err => {
+      console.log("error: ", err);
+    })
+  }, [])
 
   return (
     <div className={quizClassNames}>
---
+++ src/utilities/utility.js
@@ -0,0 +1,70 @@
+import { decode } from "html-entities";
+import { nanoid } from "nanoid";
+
+export function mapQuestionsFromApi(questionsData) {
+    const questionsFromApi = questionsData.results;
+    const mappedQuestions = [];
+
+    questionsFromApi.forEach(questionFromApi => {      
+        const decodedQuestion = getDecodeString(questionFromApi.question);
+        const decodedIncorrectAnswers = questionFromApi.incorrect_answers.map(incorrectAnswer => {
+            return getDecodeString(incorrectAnswer);
+        })
+
+        const decodedCorrectAnswer = questionFromApi.correct_answer = getDecodeString(questionFromApi.correct_answer);
+        mappedQuestions.push({
+            question: decodedQuestion,
+            choices: prepareChoices(decodedIncorrectAnswers, decodedCorrectAnswer),
+            isAnswered: false,
+            isAnsweredCorrectly: false,
+            showAnswer: false,
+            id: nanoid()
+        })
+    })
+
+    return mappedQuestions;
+}
+
+function getDecodeString(str) {
+    return decode(str);
+}
+    
+function prepareChoices(incorrectAnswers, correctAnswer) {
+    let arr = [];
+
+    incorrectAnswers.forEach(incorrectAnswer => {
+        arr.push(
+            {
+                choice: incorrectAnswer, 
+                isSelected: false,
+                isCorrectChoice: false,
+                id: nanoid(),
+            });
+        }
+    );
+
+    arr.push(
+        {
+            choice: correctAnswer, 
+            isSelected: false,
+            isCorrectChoice: true,
+            id: nanoid(),
+        }
+    );
+
+    shuffleArray(arr);
+
+    return arr;
+}
+
+// using the Fisher-Yates algorith in combination with Math.Random()
+// see: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+// see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
